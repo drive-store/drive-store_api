5277516e9e4ea666a338106d35b6b13946a895c4
Germain Lefebvre <germain.lefebvre4@gmail.com>
2018-09-12 00:19:19 +0200

    feat(companies): Finish companies methods (to perform)

--- companies/views.py
+++ companies/views.py
@@ -10,6 +10,7 @@ import io, json
 def index(request):
     return HttpResponse("Hello, world. You're at the polls index.")
 
+# http://localhost:8000/companies
 def getCompanies(requests):
     client = MongoClient("localhost", 27017)
     db = client['auchan-products']
@@ -19,14 +20,12 @@ def getCompanies(requests):
     json_data = cur_companies
     return JsonResponse(json_data, safe=False, json_dumps_params={'ensure_ascii':False})
 
+# http://localhost:8000/companies/Auchan
 def getCompanyByName(requests, company_name):
     json_return = {}
     return JsonResponse(json_return, safe=False,json_dumps_params={'ensure_ascii':False})
 
-def getCompanyProductDetails(request, company_name, product_name):
-    json_return = {}
-    return JsonResponse(json_return, safe=False,json_dumps_params={'ensure_ascii':False})
-
+# http://localhost:8000/companies/Auchan/products
 def getCompanyProducts(requests, company_name):
     json_return = []
     client = MongoClient("localhost", 27017)
@@ -38,6 +37,7 @@ def getCompanyProducts(requests, company_name):
         json_return.append(d)
     return JsonResponse(json_return, safe=False,json_dumps_params={'ensure_ascii':False})
 
+# http://localhost:8000/companies/Auchan/products/Jardin%20Bio%20pur%20jus%20de%20citron%20vert%2025cl
 def getCompanyProductByName(requests, company_name, product_name):
     json_return = []
     client = MongoClient("localhost", 27017)
@@ -50,6 +50,33 @@ def getCompanyProductByName(requests, company_name, product_name):
         json_return.append(d)
     return JsonResponse(json_return, safe=False,json_dumps_params={'ensure_ascii':False})
 
+# http://localhost:8000/companies/Auchan/products/Jardin%20Bio%20pur%20jus%20de%20citron%20vert%2025cl/prices
+def getCompanyProductPrices(requests, company_name, product_name):
+    json_return = []
+    client = MongoClient("localhost", 27017)
+    db = client['auchan-products']
+    db.authenticate("scrapy59", "scrapy59")
+    collection = db['products']
+    print(product_name)
+    cur_products = collection.find({"Company": company_name, "Product": product_name}, {"_id": 0, "Location": 1, "Price": 1}).sort("Date", 1)
+    for d in cur_products:
+        json_return.append(d)
+    return JsonResponse(json_return, safe=False,json_dumps_params={'ensure_ascii':False})
+
+# http://localhost:8000/companies/Auchan/products/Jardin%20Bio%20pur%20jus%20de%20citron%20vert%2025cl/locations/best-price
+def getCompanyProductBestPrice(requests, company_name, product_name):
+    json_return = []
+    client = MongoClient("localhost", 27017)
+    db = client['auchan-products']
+    db.authenticate("scrapy59", "scrapy59")
+    collection = db['products']
+    print(product_name)
+    cur_products = collection.find({"Company": company_name, "Product": product_name}, {"_id": 0, "Location": 1, "Price": 1}).sort("Date", 1).limit(1)
+    for d in cur_products:
+        json_return.append(d)
+    return JsonResponse(json_return, safe=False,json_dumps_params={'ensure_ascii':False})
+
+# http://localhost:8000/companies/Auchan/products/Jardin%20Bio%20pur%20jus%20de%20citron%20vert%2025cl/locations
 def getCompanyProductLocations(requests, company_name, product_name):
     json_return = []
     client = MongoClient("localhost", 27017)
@@ -62,59 +89,87 @@ def getCompanyProductLocations(requests, company_name, product_name):
         json_return.append(d)
     return JsonResponse(json_return, safe=False,json_dumps_params={'ensure_ascii':False})
 
-def getCompanyProductPrices(requests, company_name, product_name):
+# http://localhost:8000/companies/Auchan/products/Jardin%20Bio%20pur%20jus%20de%20citron%20vert%2025cl/locations/Englos/price-history
+def getCompanyProductLocationPriceHistory(requests, company_name, product_name, location_name):
     json_return = []
     client = MongoClient("localhost", 27017)
     db = client['auchan-products']
     db.authenticate("scrapy59", "scrapy59")
     collection = db['products']
     print(product_name)
-    cur_products = collection.find({"Company": company_name, "Product": product_name}, {"_id": 0, "Location": 1, "Price": 1})
+    cur_products = collection.find({"Company": company_name, "Product": product_name, "Location": location_name}, {"_id": 0, "Date": 1, "Price": 1})
     for d in cur_products:
         json_return.append(d)
     return JsonResponse(json_return, safe=False,json_dumps_params={'ensure_ascii':False})
 
-def getCompanyProductBestPrice(requests, company_name, product_name):
-    json_return = []
-    client = MongoClient("localhost", 27017)
-    db = client['auchan-products']
-    db.authenticate("scrapy59", "scrapy59")
-    collection = db['products']
-    print(product_name)
-    cur_products = collection.find({"Company": company_name, "Product": product_name}, {"_id": 0, "Location": 1, "Price": 1}).sort([("Price", 1)]).limit(1)
-    print(cur_products)
-    for d in cur_products:
-        json_return.append(d)
-    return JsonResponse(json_return, safe=False,json_dumps_params={'ensure_ascii':False})
-
-def getCompanyProductHistory(requests, company_name, product_name):
-    json_return = {}
-    return JsonResponse(json_return, safe=False,json_dumps_params={'ensure_ascii':False})
-
+# http://localhost:8000/companies/Auchan/locations
 def getCompanyLocations(requests, company_name):
-    json_return = {}
+    json_return = []
+    client = MongoClient("localhost", 27017)
+    db = client['auchan-products']
+    db.authenticate("scrapy59", "scrapy59")
+    collection = db['products']
+    cur_products = collection.find({"Company": company_name}, {"_id": 0, "Location": 1}).distinct("Location")
+    for d in cur_products:
+        json_return.append(d)
     return JsonResponse(json_return, safe=False,json_dumps_params={'ensure_ascii':False})
 
-def getCompanyLocationByName(requests, company_name):
-    json_return = {}
+# http://localhost:8000/companies/Auchan/locations/Englos
+def getCompanyLocationByName(requests, company_name, location_name):
+    json_return = []
+    client = MongoClient("localhost", 27017)
+    db = client['auchan-products']
+    db.authenticate("scrapy59", "scrapy59")
+    collection = db['products']
+    cur_products = collection.find({"Company": company_name, "Location": location_name}, {"_id": 0, "Location": 1})
+    for d in cur_products:
+        json_return.append(d)
     return JsonResponse(json_return, safe=False,json_dumps_params={'ensure_ascii':False})
 
-def getCompanyLocationProducts(requests, company_name):
-    json_return = {}
+# http://localhost:8000/companies/Auchan/locations/Englos/products
+def getCompanyLocationProducts(requests, company_name, location_name):
+    json_return = []
+    client = MongoClient("localhost", 27017)
+    db = client['auchan-products']
+    db.authenticate("scrapy59", "scrapy59")
+    collection = db['products']
+    cur_products = collection.find({"Company": company_name, "Location": location_name}, {"_id": 0})
+    for d in cur_products:
+        json_return.append(d)
     return JsonResponse(json_return, safe=False,json_dumps_params={'ensure_ascii':False})
 
-def getCompanyLocationProductByName(requests, company_name, product_name):
-    json_return = {}
+# http://localhost:8000/companies/Auchan/locations/Englos/products/Jardin%20Bio%20pur%20jus%20de%20citron%20vert%2025cl
+def getCompanyLocationProductByName(requests, company_name, location_name, product_name):
+    json_return = []
+    client = MongoClient("localhost", 27017)
+    db = client['auchan-products']
+    db.authenticate("scrapy59", "scrapy59")
+    collection = db['products']
+    cur_products = collection.find({"Company": company_name, "Location": location_name, "Product": product_name}, {"_id": 0}).sort("Date", 1).limit(1)
+    for d in cur_products:
+        json_return = d
     return JsonResponse(json_return, safe=False,json_dumps_params={'ensure_ascii':False})
 
-def getCompanyLocationProductPrices(requests, company_name, product_name):
-    json_return = {}
+# http://localhost:8000/companies/Auchan/locations/Englos/products/Jardin%20Bio%20pur%20jus%20de%20citron%20vert%2025cl/prices
+def getCompanyLocationProductPrices(requests, company_name, location_name, product_name):
+    json_return = []
+    client = MongoClient("localhost", 27017)
+    db = client['auchan-products']
+    db.authenticate("scrapy59", "scrapy59")
+    collection = db['products']
+    cur_products = collection.find({"Company": company_name, "Location": location_name, "Product": product_name}, {"_id": 0, "Price": 1}).sort("Date", 1).limit(1)
+    for d in cur_products:
+        json_return = d
     return JsonResponse(json_return, safe=False,json_dumps_params={'ensure_ascii':False})
 
-def getCompanyLocationBestPrice(requests, company_name, product_name):
-    json_return = {}
-    return JsonResponse(json_return, safe=False,json_dumps_params={'ensure_ascii':False})
-
-def getCompanyLocationPriceHistory(requests, company_name, product_name):
-    json_return = {}
+def getCompanyLocationPriceHistory(requests, company_name, location_name, product_name):
+    json_return = []
+    client = MongoClient("localhost", 27017)
+    db = client['auchan-products']
+    db.authenticate("scrapy59", "scrapy59")
+    collection = db['products']
+    print(product_name)
+    cur_products = collection.find({"Company": company_name, "Product": product_name, "Location": location_name}, {"_id": 0, "Date": 1, "Price": 1})
+    for d in cur_products:
+        json_return.append(d)
     return JsonResponse(json_return, safe=False,json_dumps_params={'ensure_ascii':False})
